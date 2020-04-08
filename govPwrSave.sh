89f1eae52622ef313d752fa2ac3fec3585f40bda
melvinga <melvingeorge@gmail.com>
2020-04-08 14:43:40 +0530

    use nproc to auto-loop over all cores

--- govPwrSave.sh
+++ govPwrSave.sh
@@ -2,35 +2,33 @@
 
 # FOR i5-8265U
 GOVERNOR_VALUE=powersave
-MIN_FREQ_VALUE=400000
-MAX_FREQ_VALUE=1600000
+MAX_FREQ_VALUE=$(( 16 * 100000 ))
+MIN_FREQ_VALUE=$((  4 * 100000 ))
 
-sudo cpufreq-set --cpu 0 --governor $GOVERNOR_VALUE 
-sudo cpufreq-set --cpu 1 --governor $GOVERNOR_VALUE 
-sudo cpufreq-set --cpu 2 --governor $GOVERNOR_VALUE 
-sudo cpufreq-set --cpu 3 --governor $GOVERNOR_VALUE 
-sudo cpufreq-set --cpu 4 --governor $GOVERNOR_VALUE 
-sudo cpufreq-set --cpu 5 --governor $GOVERNOR_VALUE 
-sudo cpufreq-set --cpu 6 --governor $GOVERNOR_VALUE 
-sudo cpufreq-set --cpu 7 --governor $GOVERNOR_VALUE 
+FILE=/usr/bin/cpufreq-set
+if ! [ -f "$FILE" ]; then
+  sudo apt update && sudo apt -y install cpufrequtils
+fi
 
-echo "Governor is set to:"
-cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-echo ""
+if [ -f "$FILE" ]; then
+  for ((i=0;i<$(nproc);++i)); do sudo cpufreq-set -c $i -r -g $GOVERNOR_VALUE; done
 
-sudo cpufreq-set --cpu 0 --min $MIN_FREQ_VALUE --max $MAX_FREQ_VALUE
-sudo cpufreq-set --cpu 1 --min $MIN_FREQ_VALUE --max $MAX_FREQ_VALUE
-sudo cpufreq-set --cpu 2 --min $MIN_FREQ_VALUE --max $MAX_FREQ_VALUE
-sudo cpufreq-set --cpu 3 --min $MIN_FREQ_VALUE --max $MAX_FREQ_VALUE
-sudo cpufreq-set --cpu 4 --min $MIN_FREQ_VALUE --max $MAX_FREQ_VALUE
-sudo cpufreq-set --cpu 5 --min $MIN_FREQ_VALUE --max $MAX_FREQ_VALUE
-sudo cpufreq-set --cpu 6 --min $MIN_FREQ_VALUE --max $MAX_FREQ_VALUE
-sudo cpufreq-set --cpu 7 --min $MIN_FREQ_VALUE --max $MAX_FREQ_VALUE
+  echo "Governor is set to:"
+  cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+  echo ""
 
-echo "Min freq is set to:"
-cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq
-echo ""
+  for ((i=0;i<$(nproc);++i)); do sudo cpufreq-set -c $i --min $MIN_FREQ_VALUE --max $MAX_FREQ_VALUE; done
 
-echo "Max freq is set to:"
-cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
-echo ""
+  echo "Min freq is set to:"
+  cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq
+  echo ""
+
+  echo "Max freq is set to:"
+  cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
+  echo ""
+
+  echo "Press Ctrl+C to exit"
+  sleep 5
+  watch -n1 "cat /proc/cpuinfo | grep \"^[c]pu MHz\""
+  echo ""
+fi
